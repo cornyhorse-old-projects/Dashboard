3df0e3423679e49f55137da9e84cfee63d099f64
Alisha Mohanty <alishamohanty1416@gmail.com>
2019-09-28 00:19:21 +0530

    Refactor Code
    1.Remove unnecessary imports
    2. Remove redundant code
    3. Rename URLS to have consisitent naming style
    4. Solve linting issues

--- dashboard/dashboard/compute/urls.py
+++ dashboard/dashboard/compute/urls.py
@@ -4,13 +4,13 @@ from .views import ResourceListView, ResourceCreateView, NetworkCreateView, Reso
 
 
 urlpatterns = [
-     path('', ResourceListView.as_view(), name='ComputeResources'),
-     path('create/', ResourceCreateView.as_view(), name='AddComputeResource'),
-     path('add-network/', NetworkCreateView.as_view(), name='Add-Network'),
+     path('', ResourceListView.as_view(), name='networkResources'),
+     path('create/', ResourceCreateView.as_view(), name='add_networkResource'),
+     path('add-network/', NetworkCreateView.as_view(), name='add_network'),
      path('<int:pk>/', ResourceDetailView.as_view(), name='resource_detail'),
-     path('<int:pk>/edit', NetworkResourceUpdate.as_view(), name='networkResource_edit'),
+     path('<int:pk>/update', NetworkResourceUpdate.as_view(), name='networkResource_update'),
      path('<int:pk>/delete', NetworkResourceDelete.as_view(), name='networkResource_delete'),
      path('network/<int:pk>', NetworkDetailView.as_view(), name="network_detail"),
-     path('network/<int:pk>/edit', NetworkUpdate.as_view(), name="network_edit"),
+     path('network/<int:pk>/update', NetworkUpdate.as_view(), name="network_update"),
      path('network/<int:pk>/delete', NetworkDelete.as_view(), name="network_delete")
 ]
--- dashboard/dashboard/compute/views.py
+++ dashboard/dashboard/compute/views.py
@@ -1,17 +1,9 @@
-from django.shortcuts import render
 from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse
-from django.views.generic import DetailView, RedirectView, UpdateView
-from django.contrib import messages
-from django.utils.translation import ugettext_lazy as _
-from . import models as m
+from .models import ComputeResource, Network
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserChangeForm
 from django.shortcuts import redirect
-import logging
 from .forms import ResourceCreateForm, NetworkForm
 
 
@@ -20,79 +12,40 @@ User = get_user_model()
 
 # Create your views here.
 class ResourceListView(LoginRequiredMixin, ListView):
-    model = (
-        m.ComputeResource
-    )  # .objects.select_related('network').filter(network__isnull=True)
-
-    # def get_queryset(self):
-    #     qs = (super(ResourceListView, self).get_queryset().all())
-    #     logging.info("Printing qs")
-    #     logging.info(qs)
-    #           # prefetch_related('Network', 'NetworkOwner').filter(network__networkowner__owner=self.request.user))
-    #     #logging.info("^^^^^^^^^^^^^^^^^")
-    #     #logging.info(m.Network.objects.all().select_related('owners').filter(owners=self.request.user))
-    #     return qs
+    model = ( ComputeResource )
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ResourceListView, self).get_context_data(**kwargs)
-        #context["network"] = m.Network.objects.distinct()
-        #context["network"] = m.Network.objects.all()
-        context["network"] = m.Network.objects.select_related('owners').filter(owners=self.request.user)
-        #logging.info(m.Network.objects.all().filter(owners=self.request.user).query)
-            # .filter(owners=self.request.user)
-        #logging.info("---------")
-        #logging.info(context)
+        context["network"] = Network.objects.select_related('owners').filter(owners=self.request.user)
         return context
 
 
 class NetworkCreateView(LoginRequiredMixin, CreateView):
-    model = m.Network
+    model = Network
     login_url = "login"
     form_class = NetworkForm
 
-    success_url = reverse_lazy("ComputeResources")
+    success_url = reverse_lazy("networkResources")
 
     def form_valid(self, form):
         form.instance.owners = self.request.user
-        return super(NetworkCreateView,self).form_valid(form)
-
-
+        return super(NetworkCreateView, self).form_valid(form)
 
 
 class ResourceCreateView(LoginRequiredMixin, CreateView):
-    model = m.ComputeResource
-    form_class=ResourceCreateForm
-    template_name='computeresource_form.html'
+    model = ComputeResource
+    form_class = ResourceCreateForm
+    template_name = 'computeresource_form.html'
+    success_url = reverse_lazy("networkResources")
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         kwargs['user'] = self.request.user
         return kwargs
-    #login_url = "login"
-    # fields = (
-    #      "network",
-    #      "resource_name",
-    #      "portable",
-    #      "local_ipv4_address",
-    #      "local_ipv6_address",
-    #      "external_ipv4_address",
-    #      "external_ipv6_address",
-    #      "powered_on",
-    #      "powered_on_utc",
-    #      "uptime",
-    #      "last_seen",
-    #      "description",
-    #  )
-    # #form = PersonForm()
-    success_url = reverse_lazy("ComputeResources")
-
-    # def form_valid(self, form):
-    #     form.instance.owner = self.request.user
-    #     return super().form_valid(form)
 
 
 class ResourceDetailView(LoginRequiredMixin, DetailView):
-    model = m.ComputeResource
+    model = ComputeResource
     login_url = 'login'
 
     def get_context_data(self, **kwargs):
@@ -100,32 +53,8 @@ class ResourceDetailView(LoginRequiredMixin, DetailView):
         return context
 
 
-# def networkResourceUpdate(request, id):
-    # form_class = ResourceCreateView
-    # model = m.ComputeResource
-    # template_name = 'templates/computersource_form.html'
-
-    # def __init__(self, **kwargs):
-    #     super().__init__(**kwargs)
-    #     self.object = m.ComputeResource.objects.get(id=self.kwargs['id'])
-    #
-    # def get(self, request, *args, **kwargs):
-    #     form_class = self.get_form_class()
-    #     form = self.get_form_class(form_class)
-    #     context = self.get_context_data(object=self.object, form=form)
-    #     return self.render_to_response(context)
-    # if request.method == 'POST':
-    #     networkInstance =
-    #     form = UserChangeForm(request.POST, instance=request.user)
-    #
-    #         if form.is_valid():
-    #             form.save()
-    #             return redirect('update-network-details', id=request.parse_context['kwargs']['id'])
-    #
-    # pass
-
 class NetworkResourceUpdate(LoginRequiredMixin, UpdateView):
-    model = m.ComputeResource
+    model = ComputeResource
     form_class = ResourceCreateForm
     template_name = 'compute/computeresource_update.html'
 
@@ -139,21 +68,24 @@ class NetworkResourceUpdate(LoginRequiredMixin, UpdateView):
         self.object.save()
         return redirect('resource_detail', pk=self.kwargs['pk'])
 
+
 class NetworkResourceDelete(LoginRequiredMixin, DeleteView):
-    model = m.ComputeResource
-    success_url = reverse_lazy('ComputeResources')
+    model = ComputeResource
+    success_url = reverse_lazy('networkResources')
     template_name = 'compute/resource_delete.html'
 
+
 class NetworkDetailView(LoginRequiredMixin, DetailView):
-    model = m.Network
+    model = Network
     login_url = 'login'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
 
+
 class NetworkUpdate(LoginRequiredMixin, UpdateView):
-    model = m.Network
+    model = Network
     form_class = NetworkForm
     template_name = 'compute/network_update.html'
 
@@ -162,7 +94,8 @@ class NetworkUpdate(LoginRequiredMixin, UpdateView):
         self.object.save()
         return redirect('network_detail', pk=self.kwargs['pk'])
 
+
 class NetworkDelete(LoginRequiredMixin, DeleteView):
-    model = m.Network
-    success_url = reverse_lazy('ComputeResources')
+    model = Network
+    success_url = reverse_lazy('networkResources')
     template_name = 'compute/resource_delete.html'
